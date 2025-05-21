ca2e4017df2cf3ccf77e07f64db7b4c55c9a0df1
Deepesh <deepeshdangi700@gmail.com>
2025-05-21 21:09:20 +0530

    Register api

---
+++ Backend/routes/auth.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const User = require("../Models/User");
+const bcypt = require('bcrypt');
+const {getToken} = require('../utils/helpers');
+
+router.post('/register', async(req, res) => {
+
+    const {email, password, firstName, lastName, username} = req.body;
+
+    const user = await User.findOne({email:email});
+    if (user) { 
+        return res
+            .status(403)
+            .json({error: "A user with this email already exists"});
+
+    }
+
+    const hashedPassword = bcrypt.hash(password, 10);
+    const newUserData = {
+        email, 
+        password: hashedPassword, 
+        firstName,
+        lastName, 
+        username,
+    };
+    const newUser  = await User.create(newUserData);
+    const token = getToken(email, newUser);
+
+    const userToReturn = {...newUser.toJSON(), token};
+    delete userToReturn.password;
+    return res.status(200).json(userToReturn);
+
+})
