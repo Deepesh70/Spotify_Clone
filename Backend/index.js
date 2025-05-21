819f9b547e1452c564cd91a0e0e80b71754ca80c
Deepesh <deepeshdangi700@gmail.com>
2025-05-21 19:48:16 +0530

    Passport added

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+const passport = require('passport');
+const User = require('./Models/User');
 const app = express();
 const port = 8000;
 
@@ -27,3 +31,24 @@ app.get('/', (req, res) =>{
 app.listen(port, () =>{
     console.log('Server is running on port ' + port);
 });
+
+//passport jwt
+
+let opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'secret';
+opts.issuer = 'accounts.examplesoft.com';
+opts.audience = 'yoursite.net';
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    User.findOne({id: jwt_payload.sub}, function(err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));
