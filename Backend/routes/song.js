ab87bb0f0a1403817216b6124317da800a78b42d
Deepesh <deepeshdangi700@gmail.com>
2025-05-25 22:43:12 +0530

    songs api

--- Backend/routes/song.js
+++ Backend/routes/song.js
@@ -9,7 +9,7 @@ router.post("/create", passport.authenticate("jwt",{session:false}), async (req,
         return res 
             .status(301)
             .json({error: "Title, thumbnail, and track are required"});
-    }
+    }  
     const artist = req.user._id;
     const songDetails = {title, thumbnail, track, artist};
     const createSong = await Song.create(songDetails);
@@ -20,7 +20,7 @@ router.post("/create", passport.authenticate("jwt",{session:false}), async (req,
 router.get("/get/mysongs",
     passport.authenticate("jwt", {session:false}),
     async (req, res) => {
-        const currentUser = req.user;
+      
         const songs = await Song.find({artist: req.user._id});
         return res.status(200).json({data: songs});
     }
